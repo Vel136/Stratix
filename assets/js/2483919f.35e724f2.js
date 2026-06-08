281932d6d46bdd86b5125ddb5ae1cc122682ff2b
Velix <putrayogamdn@gmail.com>
2026-06-08 11:38:00 +0700

    Built and published by Moonwave

---
+++ assets/js/2483919f.35e724f2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[529],{7676:e=>{e.exports=JSON.parse('[{"type":"category","label":"API","collapsible":true,"collapsed":true,"items":[{"type":"link","href":"/api/Stratix","label":"\u200bStratix"}]}]')}}]);
---
+++ assets/js/5c32e2a4.638ea005.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[562],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/Vel136/Stratix/blob/master","baseUrl":"/Stratix/","classOrder":[{"section":"API","classes":["Stratix"]}],"apiCategories":[]}')}}]);
